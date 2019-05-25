db4f13feebec4f9d6a22514dc9b0e5662b44c41b
kariokax <felipe.c.machado123@gmail.com>
2019-05-25 04:53:10 -0300

    Entidade, negócio e persistência gênero

--- src/persistencia/PGenero.java
+++ src/persistencia/PGenero.java
@@ -1,15 +1,42 @@
 package persistencia;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import entidade.Genero;
 
 public class PGenero {
 
+	Connection cnn;
 	
-	public void incluir(Genero genero, Connection cnn) throws SQLException {
+	public PGenero() {
+		cnn = util.Conexao.getConexao();
+	}
+	
 		
-		
-}
+	public List<Genero> listarGenero() throws SQLException {
+        String sql = "SELECT * FROM genero";
+        
+        Statement stm = cnn.createStatement();
+        
+        ResultSet rs = stm.executeQuery(sql);
+        
+        List<Genero> lista = new ArrayList<>();
+        
+        while (rs.next()) {
+            Genero genero = new Genero();
+            genero.setId(rs.getInt("id"));
+            genero.setGenero(rs.getString("nome_genero"));
+            
+            lista.add(genero);
+        }
+        rs.close();
+        cnn.close();
+        
+        return lista;
+    }
+
 }
