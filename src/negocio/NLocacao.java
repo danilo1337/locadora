087fb795630f01936e1303cd4ce7aec475601fa7
DavidGustavoRX <ExtFuture1@gmail.com>
2019-05-26 02:04:42 -0300

    Refatorado NPedido para NLocacao

--- src/negocio/NLocacao.java
+++ src/negocio/NLocacao.java
@@ -3,15 +3,48 @@ package negocio;
 import entidade.Locacao;
 import persistencia.PLocacao;
 
-public class NPedido {
+import java.util.List;
+
+public class NLocacao {
+
+    private PLocacao per;
+
+    public NLocacao(){
+        per = new PLocacao();
+    }
 
     public void salvar(Locacao parametro) throws Exception {
 
-        if (parametro.getId()== 0) {
+
+
+        if (parametro.getValor_total() == 0 && parametro.getListaItens() == null)
+            throw new Exception("Por favor, Inserir algum produto");
+
+
+        if (parametro.getListaItens() == null)
+            throw new Exception("É necessário inserir um item");
+
+
+        if (parametro.getId()== 0)
             new PLocacao().incluir(parametro);
-        } else {
+         else
             new PLocacao().alterar(parametro);
-        }
+
     }
-//isso precisa tambem
+
+    public void excluir(Locacao locacao) throws Exception{
+        per.excluir(locacao);
+    }
+
+    public Locacao consultar(int id) throws Exception {
+        return per.consultar(id);
+    }
+
+    public List<Locacao> listar() throws Exception {
+        return per.listar();
+    }
+
+
+
+
 }
